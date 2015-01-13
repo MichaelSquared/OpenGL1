ea1eb3ae2f9d6732382660086b8a2c9bc232e6d4
MichaelSquared <mixailmixail@live.com>
2015-01-13 16:37:47 +0000

    Created a triangle

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 #include <GL/gl.h>
 #include <cstdlib>
 #include <GL/glu.h>
+
+void drawTriangle();
+
+
 int main()
 {
     if(SDL_Init(SDL_INIT_VIDEO))
@@ -42,8 +46,61 @@ int main()
     glMatrixMode(GL_MODELVIEW);
     gluLookAt(2,2,2,0,0,0,0,1,0);
 
-    SDL_GL_SwapWindow(window);
-    SDL_Delay(10000);
+    bool quit=false;
+    SDL_Event event;
+    while(!quit)
+    {
+        while(SDL_PollEvent(&event))
+        {
+            switch( event.type)
+            {
+                case SDL_QUIT : quit =true; break;
+            }
+        }
+
+        drawTriangle();
+        SDL_GL_SwapWindow(window);
+
+    } //end quit loop
+
+
 
 
 }
+
+void drawTriangle()
+{
+    static int rot=0;
+    glPushMatrix();
+      glBegin(GL_TRIANGLES);
+        glColor3f(1.0f, 0.0, 0.0f);
+        glVertex3f(0.0f, 1.0f, 0.0f);
+        glColor3f(0.0f, 1.0f, 0.0f);
+        glVertex3f(1.0f, 1.0f, 0.0f);
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glVertex3f(-1.0f, -1.0f, 0.0f);
+      glEnd();
+     glPopMatrix();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
